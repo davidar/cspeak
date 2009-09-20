2f24c1d31e63673bdb4ed9323d73026fecdb7c83
David Roberts <d@vidr.cc>
2009-09-20 16:46:39 +1000

    fix typo in reciter.sh

--- reciter.sh
+++ reciter.sh
@@ -6,4 +6,4 @@ SOX_ARGS="-t raw -b 32 -e signed-integer -r 44100"
 make && python reciter.py $@ | ./reciter | play $SOX_ARGS -
 
 # convert to Ogg Vorbis before playing
-#make && python reciter.py $@ | ./reciter | sox $SOX_ARGS - daisy.ogg && play daisy.ogg
+#make && python reciter.py $@ | ./reciter | sox $SOX_ARGS - reciter.ogg && play reciter.ogg
